4da4a87f9d8de6730f1a15ed20bbc7c72b55197e
Chris Nelson <chnelson@C02TP1TRHTD8.local>
2017-07-15 15:00:25 -0400

    Initial commit of working run test in terminal command

---
+++ spec/run-test-in-terminal-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RunTestInTerminalView from '../lib/run-test-in-terminal-view';
+
+describe('RunTestInTerminalView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
